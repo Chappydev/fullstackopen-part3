18129e4f72ce86cf78aa337310e96c8b5c1f52f8
Chappydev <danjtchapman@gmail.com>
2022-09-01 16:12:56 -0600

    Complete 3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 
 morgan.token('body', (req, res) => req.body ? JSON.stringify(req.body) : '');
