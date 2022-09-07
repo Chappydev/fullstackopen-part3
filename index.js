99615aa94f838ab868c37b6e5047d39d92e415cf
Chappydev <danjtchapman@gmail.com>
2022-09-07 10:48:49 -0600

    Complete 3.19

--- index.js
+++ index.js
@@ -87,7 +87,10 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id, 
+    person, 
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       res.json(updatedPerson);
     })
@@ -99,7 +102,9 @@ const errorHandler = (err, request, response, next) => {
 
   if (err.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (err.name === 'ValidationError') {
+    return response.status(400).json({ error: err.message });
+  }
 
   next(err)
 };
